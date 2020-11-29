46d305f9f11285ee9d9f81372df66af514e9f015
Alex <akazimirov@tut.by>
2020-11-29 22:48:58 +0300

    homework#10 complete

--- Lesson-10/main.js
+++ Lesson-10/main.js
@@ -104,6 +104,203 @@ clonedObj.array.push(2);
 console.log(initialObj);
 console.log(clonedObj);
 
+
 // Задание №3
 
-Object.keys(initialObj).length
+var initialObj = {
+    string: 'Vasya',
+    number: 30,
+    boolean: true,
+    undefined: undefined,
+    null: null,
+    array: [1, 2, 3],
+    object: {
+        string2: 'Petrov',
+        object2: {
+            array2: [{}, {}]
+        },
+        object3: {}
+    },
+    method: function() {
+        alert('Hello');
+    }
+};
+
+var compareObj = {
+    string: 'Vasya',
+    number: 30,
+    boolean: true,
+    undefined: undefined,
+    null: null,
+    array: [1, 2, 3],
+    object: {
+        string2: 'Petrov',
+        object2: {
+            array2: [{}, {}]
+        },
+        object3: {}
+    },
+    method: function() {
+        alert('Hello');
+    }
+};
+
+
+function compare(initialObj, compareObj) {
+    var initialObjProp = 0, 
+        compareObjProp = 0;
+
+    if (initialObj === compareObj) {
+        return true;
+    }
+ 
+    if (initialObj == null && typeof(initialObj) != "object" && compareObj == null && typeof(compareObj) != "object") {
+    
+        return false;
+    }
+    
+    for (var prop in initialObj) {
+        initialObjProp += 1;
+    }
+
+    for (var prop in compareObj) {
+        compareObjProp += 1;
+        if (!(prop in initialObj) || !compare(initialObj[prop], compareObj[prop])) {
+            return false;        
+        }
+    }        
+    return initialObjProp == compareObjProp;
+}
+
+initialObj.object.object2.array2[1].name = 'Vasya';
+initialObj.array.push(2);
+
+compare(initialObj, compareObj);
+console.log(initialObj);
+console.log(compareObj);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Задание 3: 
+var a = {
+    string: 'Vasya',
+    number: 30,
+    boolean: true,
+    undefined: undefined,
+    null: null,
+    array: [1, 2, 3],
+    object: {
+        string2: 'Petrov',
+        object2: {
+            array2: [{}, {}]
+        },
+        object3: {}
+    },
+    method: function() {
+        alert('Hello');
+    }
+};
+
+var b = {
+    string: 'Vasya',
+    number: 30,
+    boolean: true,
+    undefined: undefined,
+    null: null,
+    array: [1, 2, 3],
+    object: {
+        string2: 'Petrov',
+        object2: {
+            array2: [{}, {}]
+        },
+        object3: {}
+    },
+    method: function() {
+        alert('Hello');
+    }
+};
+
+
+function deepEqual(a, b) {
+    if (a === b) {
+        return true;
+    }
+ 
+    if (a == null && typeof(a) != "object" && b == null && typeof(b) != "object") {
+    
+        return false;
+    }
+ 
+    var propertiesA = 0, propertiesB = 0;
+    for (var property in a) {
+        propertiesA += 1;
+    }
+    for (var property in b) {
+        propertiesB += 1;
+        if (!(property in a) || !deepEqual(a[property], b[property])) {
+            return false;        
+        }
+    }        
+    return propertiesA == propertiesB;
+}
+
+// a.object.object2.array2[1].name = 'Vasya';
+//       a.array.push(2);
+
+
+console.log(deepEqual(a, b));
+console.log(a);
+console.log(b);
