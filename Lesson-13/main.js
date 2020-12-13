3c0180ca8c02e7267ef327e4bed305686bcff3d0
Alex <akazimirov@tut.by>
2020-12-13 14:50:27 +0300

    Homework#13 complete

--- Lesson-13/main.js
+++ Lesson-13/main.js
@@ -11,28 +11,10 @@ tbody.addEventListener('click', function(event) {
     }else {
         var activeTr = event.target;
 
-        
-
-        // activeTr.innerHTML = '<input>';
-        // activeTr.firstElementChild.focus();
-
-
-        // if (activeTr.firstElementChild) {}
-
-
-
-
         if (activeTr.textContent == '') { 
             activeTr.innerHTML = '<input>';
             activeTr.firstElementChild.focus();
-        }else {
-            var inputContent = target.textContent;
-            var input = activeTr.getElementsByTagName('input')[0];
-
-            console.log(input);
-
-            input.value += inputContent;
-            input.focus();
+            input = document.getElementsByTagName('input')[0];
 
             input.addEventListener("focusout", function() {
                 activeTr.textContent = input.value;
@@ -41,67 +23,32 @@ tbody.addEventListener('click', function(event) {
         
             input.addEventListener("keydown", function(e) {
                 if (e.keyCode === 13) {
-                    activeTr.textContent = input.value;
+                    activeTr.inputContent = input.value;
+                    input.remove();
+                }
+            });
+
+        }else {
+            var inputContent =  activeTr.textContent;
+
+            activeTr.innerHTML = '<input>';
+            input = document.getElementsByTagName('input')[0];
+            input.value += inputContent;
+            activeTr.firstElementChild.focus();
+
+            input.addEventListener("focusout", function() {
+                activeTr.textContent = input.value;
+                input.remove();
+            });
+        
+            input.addEventListener("keydown", function(e) {
+                if (e.keyCode === 13) {
+                    activeTr.inputContent = input.value;
                     input.remove();
                 }
             });
         }
-
-        // var input = activeTr.focus();
-        
-        // input.getElementsByTagName('input')[0];
-        
-        // input.getElementsByTagName('input')[0];
-        //     input.addEventListener('keypress', function(event) {
-        //         if(event.keyCode == 13) {
-        //             event.preventDefault();
-        //         }
-        //     });
-
-        // console.log(input);
-
-        
         
     }
+
 });
-
-
-
-
-// tableContainer[0].addEventListener("click", function(event) {
-//     var target = event.target;
-
-//     if (target.parentElement == idContainer){
-//       return;
-//     }
-
-//     if (target.textContent == '') {
-//         target.innerHTML = '<input type="text" size="10" id="inputId">';
-//         target.firstElementChild.focus();
-//     } else{
-//         var str = target.textContent;
-
-//         target.innerHTML = '<input type="text" size="10" id="inputId">';
-
-//         var inputElement = document.getElementById('inputId');
-
-//         inputElement.value += str;
-//         inputElement.focus();
-
-//     }
-
-//     var input = document.getElementById('inputId');
-
-//     input.addEventListener("focusout", function() {
-//         target.textContent = input.value;
-//         input.remove();
-//     });
-
-//     input.addEventListener("keydown", function(e) {
-//         if (e.keyCode === 13) {
-//             target.textContent = input.value;
-//             input.remove();
-//         }
-//     });
-
-// });
