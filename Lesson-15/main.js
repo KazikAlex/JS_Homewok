26307d397018417ff16b0a2b437aa46a3762c47e
Alex <akazimirov@tut.by>
2020-12-15 22:49:49 +0300

    Homework#15 complete

--- Lesson-15/main.js
+++ Lesson-15/main.js
@@ -1,18 +1,80 @@
 var x = document.getElementById('x'),
 y = document.getElementById('y'),
 btn = document.getElementById('btn'),
-unlockX = false,
-unlockY = false;
+order = false,
+xValidate = false,
+yValidate = false;
 
 x.addEventListener('keyup', function() {
-    unlockX = true;
-    console.log(unlockX);
+    if (x.value < 0 || x.value > 10 || x.value == ' ' || x.value.match(/\D/)) {
+        alert('Введите в поле Х число от 1 до 10');
+        x.value = '';
+        btn.setAttribute('disabled', 'disabled');
+        xValidate = false;
+    }else if (x.value == ''){
+        btn.setAttribute('disabled', 'disabled');
+        xValidate = false;
+    }else {
+        xValidate = true;
+        if (yValidate) {
+            btn.removeAttribute('disabled');
+        }
+    }
 });
-
-
 
 y.addEventListener('keyup', function() {
-    unlockY = true;
-    console.log(unlockY);
-});
+    if (y.value < 0 || y.value > 10 || y.value == ' ' || y.value.match(/\D/)) {
+        alert('Введите в поле Y число от 1 до 10');
+        y.value = '';
+        btn.setAttribute('disabled', 'disabled');
+        yValidate = false;
+    }else if (y.value == ''){
+        btn.setAttribute('disabled', 'disabled');
+        yValidate = false;
+    }else {
+        yValidate = true;
+        if (xValidate) {
+            btn.removeAttribute('disabled');
+        }
+        
+    }
+})
 
+btn.addEventListener('click', function() {
+    if (haveTable = document.getElementsByTagName('table')[0]) {
+        haveTable.remove();
+    }
+
+    var table = document.createElement('table');
+    document.body.append(table);
+    for (var i = 0; i < y.value; i++) {
+        var tr = document.createElement('tr');
+        insertTr = table.append(tr);
+        for (var j = 0; j < x.value; j++) {
+            if (j == 0 && x.value%2 == 0) {
+                order = !order;
+            }
+
+            var td = document.createElement('td');
+
+            if (order) {
+                td.className = '';
+            }else {
+                td.className = 'black';
+            }
+
+            tr.append(td);
+            order = !order;
+        }
+    }
+
+    var colTd = table.getElementsByTagName('td');
+
+    table.addEventListener('click', function(event) {
+        var target = event.target;
+        
+        for (var c = 0; c < colTd.length; c++) {
+            colTd[c].classList.toggle('black');
+        }
+    });
+});
