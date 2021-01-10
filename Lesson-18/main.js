f48a108ba6eff5d281d4062021cec93d8a171fc4
Alex <akazimirov@tut.by>
2021-01-10 16:29:17 +0300

    Homework#18 comlete

--- Lesson-18/main.js
+++ Lesson-18/main.js
@@ -1,24 +1,39 @@
 //Задача №1
 
-//    /^[a-z]{3,10}_[a-z]{3,10}-?[\d]{0,4}@[\w.-]{2,20}\.com$/.test('name_surname-1234@gmail.com');
-
 /^[a-z]{3,10}_[a-z]{3,10}(-?\d{0,4})?@([a-z\d](\.|\-)?){2,20}.com$/.test('name_surname-1234@gmail.com');
 
 
 //Задача №2
 
-var num1 = "+375-25-777-77-77";
-// num2 = 375299999999,
-// num3 = 8-044-444-44-44,
-// num4 = 8033-6666666;
+var num1 = '+375-33-777-77-77',
+num2 = '375299999999',
+num3 = '8-044-444-44-44',
+num4 = '8033-6666666';
 
 function regExp(phone) {
-
-    // var isValidNumber = phone.match(/^+?(375)|(80)?$/g);
-    // var isValidNumber = /^\+?((375)|(8-?0))^0-?()$/.test(num1);
-    var isValidNumber = /^\+?$/.test(num1);
+    var isValidNumber = /^((\+?375)|(8-?0))-?(?:25|29|33|44)-?[1-9]{1}\d{2}-?\d{2}-?\d{2}$/.test(phone);
     
     return console.log(isValidNumber);
 }
 
 regExp(num1);
+regExp(num2);
+regExp(num3);
+regExp(num4);
+
+
+//Задача №3
+
+var enterString = 'Введите строку на русском языке:';
+
+function vowelCounter(strEnt) {
+    var counter = strEnt.match(/[аоиеёэыуюяaeiou]/ig)
+    if (counter != null) {
+        return console.log('В этом предложении глассных ' + counter.length + ' шт.');
+    }else {
+        return console.log('В этом предложении глассных нет глассных');
+    }
+    
+}
+
+vowelCounter(enterString);
